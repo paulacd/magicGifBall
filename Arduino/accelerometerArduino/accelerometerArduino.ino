daf2f2bf1d62360b95f0dd6188a02ed2c5743529
paulacd <paulaceballos@172-16-250-177.DYNAPOOL.NYU.EDU>
2016-04-05 18:09:56 -0400

    added initial accelerometer ranges

--- Arduino/accelerometerArduino/accelerometerArduino.ino
+++ Arduino/accelerometerArduino/accelerometerArduino.ino
@@ -15,9 +15,6 @@ int ov3= 0;
 void setup() {
   // initialize serial communications at 9600 bps:
   Serial.begin(9600);
-
-
-  // regular: ~520, ~510, ~630
  
 }
 
@@ -39,20 +36,41 @@ void loop() {
   sv3 = analogRead(ap3);            
   
   ov3 = map(sv3, 0, 1023, 0, 255);  
+
+  if (sv1 > 550 || sv1 < 450) {
+    Serial.println(1);
+  }
+  else {
+    Serial.println(0);
+  }
+
+    if (sv2 > 600 || sv2 < 400) {
+    Serial.println(1);
+  }
+  else {
+    Serial.println(0);
+  }
+
+    if (sv3 > 850 || sv3 < 450) {
+    Serial.println(1);
+  }
+  else {
+    Serial.println(0);
+  }
   
   // print the results to the serial monitor:
   //Serial.print("Xsensor1 = " );                       
-  //Serial.print(sv1);      
+  Serial.print(sv1);      
 //  Serial.print("\t output1 = ");      
 //  Serial.println(ov1);   
 // 
-//  Serial.print("," );                       
-//  Serial.print(sv2);      
+  Serial.print("," );                       
+  Serial.print(sv2);      
 //  Serial.print("\t output2 = ");      
 //  Serial.println(ov2);   
 
-//  Serial.print("," );                       
-//  Serial.print(sv3);      
+  Serial.print("," );                       
+  Serial.print(sv3);      
 //  Serial.print("\t output3 = ");      
 //  Serial.println(ov3);   
 
